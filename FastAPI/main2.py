fe285f1c580222490060f8c6b80e68e4d7cdc4df
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-11-14 19:39:41 +0330

    'editfastAPI'

--- FastAPI/main2.py
+++ FastAPI/main2.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI , Body
+from fastapi import FastAPI , Body, Path , Query
 from pydantic import BaseModel, Field
 from typing import Optional
 
@@ -9,14 +9,16 @@ class Book:
     title : str 
     author : str 
     description : str
-    rating : int
+    rating : int 
+    year : int
     
-    def __init__(self, id , title , author , descripttion ,rating):
+    def __init__(self, id , title , author , descripttion ,rating,year):
         self.id = id 
         self.title = title
         self.author = author
         self.description = descripttion
         self.rating = rating
+        self.year = year
 
 class BookRequest(BaseModel):
     id : Optional[int]= None 
@@ -24,17 +26,18 @@ class BookRequest(BaseModel):
     author : str = Field(min_length= 1)
     description : str= Field(min_length= 1 , max_length= 100)
     rating : int = Field(gt=0 , lt=10)
+    year :int = Field(gt = 1999,lt=2031)
 
 
 books= [
-    Book(1,'ce' , 'payam' , 'the best book..' , 2),
-    Book(2,'ce' , 'sohrab' , 'the best book..' , 5),
-    Book(3,'py programming' , 'payam' , 'the best book..' , 3),
-    Book(4,'ce' , 'payam' , 'the best book..' , 4),
-    Book(5,'ml' , 'payam' , 'the best book..' , 7),
-    Book(6,'ce' , 'payam' , 'the best book..' , 5),
-    Book(7,'dl' , 'mary' , 'the best book..' , 9),
-    Book(8,'dl' , 'ali' , 'the best book..' , 12),
+    Book(1,'ce' , 'payam' , 'the best book..' , 2,2001),
+    Book(2,'ce' , 'sohrab' , 'the best book..' , 5,2005),
+    Book(3,'py programming' , 'payam' , 'the best book..' , 3, 2015),
+    Book(4,'ce' , 'payam' , 'the best book..' , 4,2010),
+    Book(5,'ml' , 'payam' , 'the best book..' , 7,2014),
+    Book(6,'ce' , 'payam' , 'the best book..' , 5,2022),
+    Book(7,'dl' , 'mary' , 'the best book..' , 9,2023),
+    Book(8,'dl' , 'ali' , 'the best book..' , 7,2027),
 ]
 
 
@@ -44,11 +47,55 @@ def read_all_books():
     return books
 #uvicorn main2:app --reload         : ->on your commands
 
+@app.get("/books2/{book_id}")
+async def read_book(book_id:int = Path(gt=0)):
+    for book in books:
+        if book.id == book_id:
+            return book
+
+
+@app.get("/books2/")
+def read_book_by_rating(book_rating: int = Query(gt=0 , lt=8)):
+    book_to_return = []
+    for book in books:
+        if book.rating == book_rating:
+            book_to_return.append(book)
+    return book_to_return
+
+
+
 @app.post("/create-book")
 def create_book(book_request : BookRequest):
-    new_book = Book(book_request.model_dump())
+    new_book = Book(**book_request.model_dump())
     books.append((new_book))
     
 def find_book_id(book: Book):
     book.id= 0 if len(books) == 0 else books[-1].id+1
     return book
+
+
+
+@app.put("/books2/update_books")
+def update_book(book:BookRequest):
+    for i in range (len(books)):
+        if books[i].id == book.id:
+            books[i] = book
+            
+            
+@app.delete("/books_del/{book_id}")
+def delete_book(book_id:int = Path(gt=0)):
+    for i in range (len(books)):
+        if books[i].id == book_id:
+            books.pop(i)
+            break
+
+
+@app.get("/books2/publish/")
+async def read_book_publish_date(year: int = Query(gt=1999, lt=2031)):
+    return_books = []
+    for book in books:
+        if book.year == year:
+            return_books.append(book)
+    return return_books
+            
+#http://127.0.0.1:8000/docs#/
