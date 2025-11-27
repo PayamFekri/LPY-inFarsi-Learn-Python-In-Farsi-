fa72ca03fb3ec02f4b6847e5040a5dc120cfb7a6
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-11-28 00:50:35 +0330

    'create-user-table'

--- FastAPI/fastAPI by DataBase/data.py
+++ FastAPI/fastAPI by DataBase/data.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker ,declarative_base
 
-SQLAlchemy_DataBase_URL = 'sqlite:///./todos.db'
+SQLAlchemy_DataBase_URL = 'sqlite:///./todosApp.db'
 
 engine = create_engine(SQLAlchemy_DataBase_URL , connect_args={'check_same_thread':False})
 
--- FastAPI/fastAPI by DataBase/model.py
+++ FastAPI/fastAPI by DataBase/model.py
@@ -1,5 +1,17 @@
-from sqlalchemy import Column ,Integer, String, Boolean
+from sqlalchemy import Column ,Integer, String, Boolean , ForeignKey
 from data import base
+
+class Users(base):
+    __tablename__ = 'users'
+    
+    id = Column(Integer , primary_key= True , index = True)
+    Email = Column(String, unique = True)
+    UserName = Column(String, unique = True)
+    first_name= Column(String)
+    last_name = Column(String , default=False)
+    hashed_password = Column(String)
+    is_active = Column(Boolean , default= True)
+    role = Column(String)
 
 class Todo(base):
     __tablename__ = 'todos'
@@ -9,3 +21,4 @@ class Todo(base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean , default=False)
+    ownerID = Column(Integer , ForeignKey("users.id"))
