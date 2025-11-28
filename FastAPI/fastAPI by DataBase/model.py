053fede0f5a0b171d79231f1e79bb40c86d41b35
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2025-11-28 12:10:08 +0330

    'changedProject'

--- FastAPI/fastAPI by DataBase/model.py
+++ FastAPI/fastAPI by DataBase/model.py
@@ -1,24 +1,24 @@
-from sqlalchemy import Column ,Integer, String, Boolean , ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from data import base
 
 class Users(base):
     __tablename__ = 'users'
     
-    id = Column(Integer , primary_key= True , index = True)
-    Email = Column(String, unique = True)
-    UserName = Column(String, unique = True)
-    first_name= Column(String)
-    last_name = Column(String , default=False)
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
     hashed_password = Column(String)
-    is_active = Column(Boolean , default= True)
+    is_active = Column(Boolean, default=True)
     role = Column(String)
 
 class Todo(base):
     __tablename__ = 'todos'
     
-    id = Column(Integer , primary_key= True , index = True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
-    complete = Column(Boolean , default=False)
-    ownerID = Column(Integer , ForeignKey("users.id"))
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
--- FastAPI/fastAPI by DataBase/routers/auth.py
+++ FastAPI/fastAPI by DataBase/routers/auth.py
@@ -1,7 +1,52 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from passlib.context import CryptContext
+from data import session_local
+from typing import Annotated
+from sqlalchemy.orm import Session
+from model import Users
+from starlette import status
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'] , deprecated = 'auto')
 
-@router.get("/auth/")
-def get_user():
-    return {'user':'authenticated'}
+class CreateUserRequest(BaseModel):
+    username: str 
+    email : str 
+    first_name : str
+    last_name : str 
+    password : str
+    role : str
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session , Depends(get_db)]
+
+
+
+@router.post("/auth/" , status_code = status.HTTP_201_CREATED)
+def create_user(db:db_dependency,
+                create_user_request : CreateUserRequest):
+    
+    
+    create_user_request = Users(
+    email=create_user_request.email,
+    username=create_user_request.username,
+    first_name=create_user_request.first_name,
+    last_name=create_user_request.last_name,
+    hashed_password=bcrypt_context.hash(create_user_request.password),
+    is_active=True,
+    role=create_user_request.role
+    )
+    db.add(create_user_request)
+    db.commit()
+    
+@router.post("/token")
+def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm , Depends()] , db:db_dependency):
+    return 'token'
