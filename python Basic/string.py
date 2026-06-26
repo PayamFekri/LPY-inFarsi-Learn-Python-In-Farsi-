61c62ffb5b99ce2f0a71b565a4ced15d33ddd81e
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2026-06-26 23:48:55 +0330

    'neating-files'

---
+++ python Basic/string.py
@@ -0,0 +1,35 @@
+print(type(4E2))
+print(4E2)
+
+print(bin(5))
+print(bin(9))
+print(hex(5))
+print(hex(47))
+
+print('I\'m thirsty')
+print("I'm thirsty")
+
+print('Hey you!'[5])
+
+name = 'Payam Fekri'
+
+a ='pawseifneijun' + 'eljngjenmg'
+print(a)
+a = 'I am exhausted'.strip('ed')
+print(a)
+print('chrismas morning'.split())
+print('asked Meg'.replace('Meg', 'Jack'))
+print('little'.upper())
+print('PKSKSOFK'.lower())
+f = 'oh hi there'.count('e')
+print(f)
+h = 'oh hi there'.find('i')
+print(h)
+h = 'oh hi there'.index('t')
+print(h)
+
+name1 = 'paaaaa'
+name2 = 'nooooo'
+print(f'thre are {name1} & {name2}')
+print('there are {} & {}'.format(name1,name2))
+print('Hello there %s and %s' %(name1, name2))
