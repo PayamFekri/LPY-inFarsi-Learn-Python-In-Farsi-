b272eb841f159b4d847f99edd27428d280eff978
Payam Fekri <143613023+PayamFekri@users.noreply.github.com>
2026-06-26 20:47:07 +0330

    'some-added'

--- Generator.py
+++ Generator.py
@@ -1,3 +1,24 @@
 # Generator 
 # yield
 
+def count_up_to(n):
+    count = 1 
+    while count <= n :
+        yield count
+        count += 1 
+
+counter = count_up_to(4)
+print()
+print(next(counter))
+print(next(counter))
+print(next(counter))
+print(next(counter))
+# print(next(counter)) # StopIteration
+
+# Gen Expression :
+my_Gen = (x*2 for x in range(10))
+print(my_Gen)
+print(next(my_Gen))
+print(next(my_Gen))
+print(next(my_Gen))
+print(next(my_Gen))
